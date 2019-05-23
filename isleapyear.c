e3424995e295c7e68e6cb4f61f2509740806aa44
Vladimir <toussaiv@88e9fe688451.ant.amazon.com>
2019-05-23 00:58:16 -0400

    update

--- isleapyear.c
+++ isleapyear.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
 int main(){
-    int i;
-    
-    scanf("%d", &i);
+    int n[5] = {1900, 2000, 2004, 2012, 2018};
 
-    if(i % 4 == 0){
-        printf("%d was a leap year\n", i);
-    }
-    else{
-        printf("%d was not a leap year\n", i);
+    for(int i = 0; i < 5; i++){
+        if(n[i] % 4 == 0){
+            if(n[i] % 100 == 0){
+                if(n[i] % 400 == 0){
+                    printf("%d was a leap year", i);
+                }
+                else{
+                    printf("%d was not a leap year", i);
+                }
+            }
+            else{
+                printf("%d was not a leap year", i);
+            }
+        }
+        else{
+            printf("%d was not a leap year", i);
+        }
     }
     
     return 0;
+}
