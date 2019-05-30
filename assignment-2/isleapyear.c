c68f46613b5310625193f2006f3dcb553e02aef4
Vladimir <tpatrick1496@gmail.com>
2019-05-30 00:16:35 -0400

    Update

--- assignment-2/isleapyear.c
+++ assignment-2/isleapyear.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+//define functions 
+void isaleapyear(int);
+bool LeapCheck(int year);
 
 int main(int argc, char*argv[]){
-    int i;
+    int n;
     
-    scanf("%d",&i);
-
-    if(i % 4 == 0){
-        if(i % 100 == 0){
-            if(i % 400 == 0){
-                printf("%d was a leap year", i);
-            }
-            else{
-                printf("%d was not a leap year", i);
-            }
-        }
-        else{
-                printf("%d was a leap year", i);
-        }
-    }
-    else{
-        printf("%d was not a leap year", i);
-    }
+    scanf("%d",&n);    
+    isaleapyear(n);
 
     return 0;
 }
 
+bool LeapCheck(int year){
+    return year % 4 == 0 && (!(year % 100 == 0) || year % 400 == 0);
+}
+
+void isaleapyear(int i){
+    if(LeapCheck(i)){
+        printf("%d was a leap year", i);
+    }
+    else{
+        printf("%d was not a leap year", i);
+    }
+}
