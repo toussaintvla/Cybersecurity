dbddfa44b184fe496cc956bc9e3bdd639110637b
Vladimir Toussaint <Vladimir@Vladimirs-MBP.home>
2019-06-03 10:57:05 -0400

    update

--- assignment-3/cpu.c
+++ assignment-3/cpu.c
@@ -3,42 +3,113 @@
 
 int main(int argc, char*argv[]){
     struct PCB ready_queue[10];
-    int queue_cnt = 0;
-    struct PCB current_process = {
-        .process_id = 1,
-        .arrival_timestamp = 1,
-        .total_bursttime = 4,
-        .execution_starttime = 1,
-        .execution_endtime = 5,
-        .remaining_bursttime = 4,
-        .process_priority = 8
-    };
-    struct PCB new_process = {
-        .process_id = 2,
-        .arrival_timestamp = 2,
-        .total_bursttime = 3,
-        .execution_starttime = 0,
-        .execution_endtime = 0,
-        .remaining_bursttime = 3,
-        .process_priority = 6
-    };
+    int queue_cnt = 3;
+    // struct PCB current_process = {
+    //     .process_id = 1,
+    //     .arrival_timestamp = 1,
+    //     .total_bursttime = 4,
+    //     .execution_starttime = 1,
+    //     .execution_endtime = 5,
+    //     .remaining_bursttime = 4,
+    //     .process_priority = 8
+    // };
+    // struct PCB new_process = {
+    //     .process_id = 2,
+    //     .arrival_timestamp = 2,
+    //     .total_bursttime = 3,
+    //     .execution_starttime = 0,
+    //     .execution_endtime = 0,
+    //     .remaining_bursttime = 3,
+    //     .process_priority = 6
+    // };
+
+    // struct PCB res = handle_process_arrival_pp(ready_queue, &queue_cnt, current_process, new_process, 2);
+    // printf("%d\n", ready_queue[0].process_id);
+    // printf("%d\n", ready_queue[0].arrival_timestamp);
+    // printf("%d\n", ready_queue[0].total_bursttime);
+    // printf("%d\n", ready_queue[0].execution_starttime);
+    // printf("%d\n", ready_queue[0].execution_endtime);
+    // printf("%d\n", ready_queue[0].remaining_bursttime);
+    // printf("%d\n", ready_queue[0].process_priority);
+    // printf("%d\n", queue_cnt);
+    // printf("%d\n", res.process_id);
+    // printf("%d\n", res.arrival_timestamp);
+    // printf("%d\n", res.total_bursttime);
+    // printf("%d\n", res.execution_starttime);
+    // printf("%d\n", res.execution_endtime);
+    // printf("%d\n", res.remaining_bursttime);
+    // printf("%d\n", res.process_priority);
+
+    // ready_queue[0].process_id = 1;
+    // ready_queue[0].arrival_timestamp = 1;
+    // ready_queue[0].total_bursttime =4;
+    // ready_queue[0].execution_starttime = 0;
+    // ready_queue[0].execution_endtime = 0;
+    // ready_queue[0].remaining_bursttime = 4;
+    // ready_queue[0].process_priority = 23;
+
+    // ready_queue[1].process_id = 2;
+    // ready_queue[1].arrival_timestamp = 1;
+    // ready_queue[1].total_bursttime = 4;
+    // ready_queue[1].execution_starttime = 0;
+    // ready_queue[1].execution_endtime = 0;
+    // ready_queue[1].remaining_bursttime = 4;
+    // ready_queue[1].process_priority = 22;
+
+    // ready_queue[2].process_id = 3;
+    // ready_queue[2].arrival_timestamp = 1;
+    // ready_queue[2].total_bursttime = 4;
+    // ready_queue[2].execution_starttime = 0;
+    // ready_queue[2].execution_endtime = 0;
+    // ready_queue[2].remaining_bursttime = 4;
+    // ready_queue[2].process_priority = 24;
+
+    struct PCB res1 = handle_process_completion_pp(ready_queue, &queue_cnt, 2);
     
-    struct PCB res = handle_process_arrival_pp(ready_queue, &queue_cnt, current_process, new_process, 2);
-    printf("%d\n", ready_queue[0].process_id);
-    printf("%d\n", ready_queue[0].arrival_timestamp);
-    printf("%d\n", ready_queue[0].total_bursttime);
-    printf("%d\n", ready_queue[0].execution_starttime);
-    printf("%d\n", ready_queue[0].execution_endtime);
-    printf("%d\n", ready_queue[0].remaining_bursttime);
-    printf("%d\n", ready_queue[0].process_priority);
-    printf("%d\n", queue_cnt);
-    printf("%d\n", res.process_id);
-    printf("%d\n", res.arrival_timestamp);
-    printf("%d\n", res.total_bursttime);
-    printf("%d\n", res.execution_starttime);
-    printf("%d\n", res.execution_endtime);
-    printf("%d\n", res.remaining_bursttime);
-    printf("%d\n", res.process_priority);
+    for(int i = 0; i < 3; i++){
+        printf("%d\n", ready_queue[i].process_id);
+        printf("%d\n", ready_queue[i].arrival_timestamp);
+        printf("%d\n", ready_queue[i].total_bursttime);
+        printf("%d\n", ready_queue[i].execution_starttime);
+        printf("%d\n", ready_queue[i].execution_endtime);
+        printf("%d\n", ready_queue[i].remaining_bursttime);
+        printf("%d\n", ready_queue[i].process_priority);
+    }
 
     return 0;
+}
+
+struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp)
+{
+    current_process.remaining_bursttime -= (timestamp - 1);
+    current_process.execution_endtime = 0;
+
+    ready_queue[*queue_cnt] = current_process;
+
+    new_process.execution_starttime = timestamp;
+    new_process.execution_endtime = new_process.remaining_bursttime + timestamp;
+
+    *queue_cnt += 1;
+
+    return new_process;
+}
+
+struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp)
+{
+}
+
+struct PCB handle_process_arrival_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int time_stamp)
+{
+}
+
+struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp)
+{
+}
+
+struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp, int time_quantum)
+{
+}
+
+struct PCB handle_process_completion_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp, int time_quantum)
+{
 }
--- assignment-3/oslabs.h
+++ assignment-3/oslabs.h
@@ -43,21 +43,21 @@ struct PTE {
 
 
 struct RCB handle_request_arrival_fcfs(struct RCB request_queue[QUEUEMAX], int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp);
-struct RCB handle_request_completion_fcfs(struct RCB request_queue[QUEUEMAX],int *queue_cnt); 
-struct RCB handle_request_arrival_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp); 
-struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt,int current_cylinder); 
+struct RCB handle_request_completion_fcfs(struct RCB request_queue[QUEUEMAX],int *queue_cnt);
+struct RCB handle_request_arrival_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp);
+struct RCB handle_request_completion_sstf(struct RCB request_queue[QUEUEMAX],int *queue_cnt,int current_cylinder);
 struct RCB handle_request_arrival_look(struct RCB request_queue[QUEUEMAX],int *queue_cnt, struct RCB current_request, struct RCB new_request, int timestamp);
-struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX],int *queue_cnt, int current_cylinder, int scan_direction); 
-struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);  
+struct RCB handle_request_completion_look(struct RCB request_queue[QUEUEMAX],int *queue_cnt, int current_cylinder, int scan_direction);
+struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);
 struct MEMORY_BLOCK first_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);  
-struct MEMORY_BLOCK worst_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id);  
-struct MEMORY_BLOCK next_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id, int last_address); 
-void release_memory(struct MEMORY_BLOCK freed_block, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt);  
-int process_page_access_fifo(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp); 
+struct MEMORY_BLOCK worst_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id); 
+struct MEMORY_BLOCK next_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id, int last_address);
+void release_memory(struct MEMORY_BLOCK freed_block, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt);
+int process_page_access_fifo(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp);
 int count_page_faults_fifo(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt);
-int process_page_access_lru(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp); 
+int process_page_access_lru(struct PTE page_table[TABLEMAX],int *handle_process_completion_pptable_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp);
 int count_page_faults_lru(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt);
-int process_page_access_lfu(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp); 
+int process_page_access_lfu(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp);
 int count_page_faults_lfu(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt);
 struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp);
 struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp);
