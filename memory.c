0fa20f304ff72a56542ec435cd1d088163fc5b33
Vladimir Toussaint <Vladimir@Vladimirs-MBP.home>
2019-06-23 18:04:58 -0400

    update

--- memory.c
+++ memory.c
@@ -5,12 +5,19 @@ struct MEMORY_BLOCK NULLBLOCK = {0,0,0,0};
 
 struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id)
 {
+    int temp;
     if(memory_map[*map_cnt].segment_size == request_size){
         return memory_map[*map_cnt];
     }
     else if(memory_map[*map_cnt].segment_size > request_size){
-
-        memory_map[*map_cnt].segment_size -= request_size;
+        memory_map[*map_cnt].start_address = 0;
+        temp = memory_map[*map_cnt].end_address - request_size;
+        memory_map[*map_cnt].end_address -= (temp - 1);  
+        memory_map[*map_cnt].segment_size -= (temp - 1);
+        memory_map[*map_cnt].process_id = process_id;
+    }
+    else{
+        return NULLBLOCK;
     }
 }
 
@@ -24,4 +31,9 @@ struct MEMORY_BLOCK worst_fit_allocate(int request_size, struct MEMORY_BLOCK mem
 
 struct MEMORY_BLOCK next_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id, int last_address)
 {
+}
+
+void release_memory(struct MEMORY_BLOCK freed_block, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt)
+{
+    
 }
