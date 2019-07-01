9075133fc06cd26da811fd8a9dc97e0539df5286
Vladimir <tpatrick1496@gmail.com>
2019-06-30 23:22:32 -0400

    update

---
+++ virtual.c
@@ -0,0 +1,21 @@
+#include "oslabs.h"
+
+struct NULLPTE = {0,0,0,0,0}
+
+int process_page_access_fifo(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp)
+{}
+
+int count_page_faults_fifo(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt)
+{}
+
+int process_page_access_lru(struct PTE page_table[TABLEMAX],int *handle_process_completion_pptable_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp)
+{}
+
+int count_page_faults_lru(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt)
+{}
+
+int process_page_access_lfu(struct PTE page_table[TABLEMAX],int *table_cnt, int page_number, int frame_pool[POOLMAX],int *frame_cnt, int current_timestamp)
+{}
+
+int count_page_faults_lfu(struct PTE page_table[TABLEMAX],int table_cnt, int refrence_string[REFERENCEMAX],int reference_cnt,int frame_pool[POOLMAX],int frame_cnt)
+{}
